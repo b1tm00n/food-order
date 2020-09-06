412ff488e185bd8ac888d0ad126a37b221dbc92f
Andrew Bay <spintheclock@gmail.com>
2020-09-06 16:45:15 +0100

    Counter jsx

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Andy when will you be making money from React?
+          Welcome to React!
         </p>
         <a
           className="App-link"
---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    render() {
+        return (
+            <div>
+             <u><h1>Hello World</h1></u>
+             <p>Some random text...</p>
+
+             
+             </div>
+        );
+    }
+}
+
+export default Counter;
